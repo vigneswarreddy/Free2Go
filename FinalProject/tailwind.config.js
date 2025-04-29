fd8635a8f3954939569206d67c355ada0b042ce0
Kamalapuram Vigneswara Reddy <150650086+vigneswarreddy@users.noreply.github.com>
2025-04-29 20:56:23 +0530

    Add files via upload

---
+++ FinalProject/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"], // Scans all HTML files in the root directory
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
